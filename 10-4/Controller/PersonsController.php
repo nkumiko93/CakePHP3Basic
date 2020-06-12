531ce0109fa01b25384cff631cd67f734e3553ca
nkumiko <kumiko@syon.co.jp>
2020-06-12 17:48:48 +0900

    Redmine #4091 10章のソースを新規追加。10章まで習得した内容更新。

---
+++ 10-4/Controller/PersonsController.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Controller;
+ 
+use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
+ 
+class PersonsController extends AppController
+ {
+    public $paginate = [
+        'limit' => 5,
+        'order' => [
+            'Persons.name' => 'asc'
+        ]
+    ];
+
+    public $helpers = [
+        'Paginator' => ['templates' => 
+            'paginator-templates']
+    ];
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
+
+    public function index()
+    {
+        $this->set('persons', $this->paginate());
+    }
+
+    public function add()
+    {
+        $person = $this->Persons->newEntity();
+        $this->set('person', $person);
+        if ($this->request->is('post')) {
+            $person = $this->Persons->patchEntity($person, $this->request->data);
+            if ($this->Persons->save($person)) {
+                return $this->redirect(['action' => 'index']);
+            }
+            if ($person->errors()){
+                $this->Flash->error('please check entered values...');
+            }
+        }
+    }
+
+    public function edit($id = null)
+    {
+        $person = $this->Persons->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            $person = $this->Persons->patchEntity($person, $this->request->data);
+            if ($this->Persons->save($person)) {
+                return $this->redirect(['action' => 'index']);
+            }
+        } else {
+            $this->set('person', $person);
+        }
+    }
+
+    public function delete($id = null)
+    {
+        $person = $this->Persons->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            if ($this->Persons->delete($person)) {
+                return $this->redirect(['action' => 'index']);
+            }
+        } else {
+            $this->set('person', $person);
+        }
+    }
+
+    public function find() {
+        $this->set('msg', null);
+        $persons = [];
+        if ($this->request->is('post')) {
+            $find = $this->request->data['find'];
+            $connection = ConnectionManager::get('default');
+            $query = 'select * from persons where ' . $find;
+            $persons = $connection->query($query)->fetchAll('assoc');
+        }
+        $this->set('persons', $persons);
+    }
+}
